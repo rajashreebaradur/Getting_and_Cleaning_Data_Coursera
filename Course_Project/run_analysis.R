ba72a84b57128f7b9ab7f0fd94d46f4e75f2e3ed
Rajashree <rajashree.baradur@gmail.com>
2016-04-12 15:37:41 -0400

    Download files
    Download all data files, extract the zipped files to data folders,
    create data and results folders

---
+++ Course_Project/run_analysis.R
@@ -0,0 +1,27 @@
+## Preparation of the material for the activities ##
+
+# Set the current working directory
+setwd("~/Coursera/Getting_and_Cleaning_Data_Coursera/Course_Project")
+currentwd <- getwd()
+
+# Download the data files
+library(httr)
+fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+destfile <- "data.zip"
+if(!file.exists(destfile)){
+        print("Downloading data files..")
+        download.file(fileurl, destfile)
+}
+
+# Unzip and create folders for data files and results
+data_folder <- "UCI HAR Dataset"
+results_folder <- "Activity Results"
+if(!file.exists(data_folder)){
+        print("Extracting data files..")
+        unzip(destfile, list = FALSE, overwrite = TRUE)
+}
+
+if(!file.exists(results_folder)){
+        print("Creating results folder..")
+        dir.create(results_folder)
+}
